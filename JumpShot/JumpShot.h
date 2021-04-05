931d3d2fb8c08a5bea4bbc28e44afa3127c45279
JerelRocktaschel <jerel.rocktaschel@gmail.com>
2021-04-05 19:07:29 -0400

    Initial Commit

---
+++ JumpShot/JumpShot.h
@@ -0,0 +1,18 @@
+//
+//  JumpShot.h
+//  JumpShot
+//
+//  Created by Jerel Rocktaschel on 4/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JumpShot.
+FOUNDATION_EXPORT double JumpShotVersionNumber;
+
+//! Project version string for JumpShot.
+FOUNDATION_EXPORT const unsigned char JumpShotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JumpShot/PublicHeader.h>
+
+
